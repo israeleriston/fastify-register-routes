a9c97a5a96c6ee1af12cc5c83230832d5554dd75
Israel E. Filgueira <israel.eriston@gmail.com>
2018-05-04 04:04:45 -0500

    commit add fastify plugin as dependecy

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+const fp = require('fastify-plugin')
 const registerRouter = require('./register-routes-by-path')
 
-module.exports = registerRouter
+function registerRoutes (instance, options, next) {
+  registerRouter(instance, options)
+  next()
+}
+
+module.exports = fp(registerRoutes, {
+  fastify: '>=1.0.0',
+  name: 'fastify-register-routes'
+})
